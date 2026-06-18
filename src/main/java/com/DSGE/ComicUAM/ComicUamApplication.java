36a9c9c7d3141dafdbca4cb1afe48ad08b5c5511
IsaacClark24 <isaacmucino08@gmail.com>
2026-06-18 10:40:49 -0600

    Se agregó la estrucuta del proyecto (MAVEN)

---
+++ src/main/java/com/DSGE/ComicUAM/ComicUamApplication.java
@@ -0,0 +1,13 @@
+package com.DSGE.ComicUAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicUamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicUamApplication.class, args);
+	}
+
+}
