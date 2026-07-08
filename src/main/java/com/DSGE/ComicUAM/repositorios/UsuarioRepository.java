de7a6c05bfde4c0cc0417c1157a7d6a65a7b68e8
IsaacClark24 <isaacmucino08@gmail.com>
2026-07-07 22:01:25 -0600

    Se cumplió parcialmente con la deuda técnica de suscripción

---
+++ src/main/java/com/DSGE/ComicUAM/repositorios/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.dsge.comicuam.repositorios;
+
+import com.dsge.comicuam.modelos.Usuario;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+}
